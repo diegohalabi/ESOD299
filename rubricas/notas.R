1106e77a1aa261f792c2e1e0394a1e615783b695
Diego Halabi <diegohalabi@gmail.com>
2018-12-16 18:51:51 -0300

    rubricas

--- rubricas/notas.R
+++ rubricas/notas.R
@@ -1,91 +1,42 @@
-#############################################################
-## Este es el script para calcular notas en ESOD299, puede ##
-##    ser utilizado, distribuido y modificado libremente   ##
-#############################################################
+#######################################################################
+####### Este es el script para calcular notas en ESOD299, puede #######
+#######    ser utilizado, distribuido y modificado libremente   #######
+#######################################################################
 # Cargar paquetes requeridos
+library(googlesheets)
 library(tidyverse)
-library(here)
-# Importa todas las evaluaciones (https://github.com/diegohalabi/ESOD299/blob/master/rubricas/rubrica_evaluacion_prof_Informante.pdf)
-here()
-df <- read.csv('eval.csv',sep=',')
-str(df)
+# Importa las evaluaciones de la base de datos
+gs_ls()
+db = 'poTEOHWvyPDFCyiGXHwLqr0f2hAqmojz'
+gs_ls(db)
+df <- gs_title(db)
+(DF_KEY <- gs_gap_key())
+third_party_gap <- DF_KEY %>%
+  gs_key()
+(DF_URL <- gs_gap_url())
+extract_key_from_url(DF_URL)
+df <- df %>%
+  gs_gs()
+df <- gs_read(df, check.names = F)
+glimpse(df)
 # Ordena los datos
 grades <- df %>% 
-  gather(nstudent,student,Estudiante.1:Estudiante.2) %>% 
-  # Reemplaza las categorías por números (puntaje)
-  # Note for DH of the future: create a function to make this code less gross.
-  mutate(R..brica..Titulo.=recode(R..brica..Titulo.,
-                                  '4. No requiere cambios en su forma actual' = 4,
-                                  '3. Requiere cambios menores' = 3,
-                                  '2. Requiere cambios mayores' = 2,
-                                  '1. No cumple con lo mínimo' = 1
-  )) %>% 
-  mutate(R..brica..Resumen.=recode(R..brica..Resumen.,
-                                  '4. No requiere cambios en su forma actual' = 4,
-                                  '3. Requiere cambios menores' = 3,
-                                  '2. Requiere cambios mayores' = 2,
-                                  '1. No cumple con lo mínimo' = 1
-  )) %>% 
-  mutate(R..brica..Introducci..n.=recode(R..brica..Introducci..n.,
-                                  '4. No requiere cambios en su forma actual' = 4,
-                                  '3. Requiere cambios menores' = 3,
-                                  '2. Requiere cambios mayores' = 2,
-                                  '1. No cumple con lo mínimo' = 1
-  )) %>% 
-  mutate(R..brica..Material.y.M..todos.A.=recode(R..brica..Material.y.M..todos.A.,
-                                  '4. No requiere cambios en su forma actual' = 4,
-                                  '3. Requiere cambios menores' = 3,
-                                  '2. Requiere cambios mayores' = 2,
-                                  '11. No cumple con lo mínimo' = 1
-  )) %>% 
-  mutate(R..brica..Material.y.M..todos.B.=recode(R..brica..Material.y.M..todos.B.,
-                                  '4. No requiere cambios en su forma actual' = 4,
-                                  '3. Requiere cambios menores' = 3,
-                                  '2. Requiere cambios mayores' = 2,
-                                  '1. No cumple con lo mínimo' = 1
-  )) %>% 
-  mutate(R..brica..Resultados.A.=recode(R..brica..Resultados.A.,
-                                  '4. No requiere cambios en su forma actual' = 4,
-                                  '3. Requiere cambios menores' = 3,
-                                  '2. Requiere cambios mayores' = 2,
-                                  '1. No cumple con lo mínimo' = 1
-  )) %>% 
-  mutate(R..brica..Resultados.B.=recode(R..brica..Resultados.B.,
-                                  '4. No requiere cambios en su forma actual' = 4,
-                                  '3. Requiere cambios menores' = 3,
-                                  '2. Requiere cambios mayores' = 2,
-                                  '1. No cumple con lo mínimo' = 1
-  )) %>% 
-  mutate(R..brica..Discusi..n.A.=recode(R..brica..Discusi..n.A.,
-                                  '4. No requiere cambios en su forma actual' = 4,
-                                  '3. Requiere cambios menores' = 3,
-                                  '2. Requiere cambios mayores' = 2,
-                                  '1. No cumple con lo mínimo' = 1
-  )) %>% 
-  mutate(R..brica..Discusi..n.B.=recode(R..brica..Discusi..n.B.,
-                                  '4. No requiere cambios en su forma actual' = 4,
-                                  '3. Requiere cambios menores' = 3,
-                                  '2. Requiere cambios mayores' = 2,
-                                  '1. No cumple con lo mínimo' = 1
-  )) %>% 
-  mutate(R..brica..Referencias.=recode(R..brica..Referencias.,
-                                  '4. No requiere cambios en su forma actual' = 4,
-                                  '3. Requiere cambios menores' = 3,
-                                  '2. Requiere cambios mayores' = 2,
-                                  '1. No cumple con lo mínimo' = 1
-  )) %>% 
-  mutate(R..brica..Escritura.=recode(R..brica..Escritura.,
-                                  '4. No requiere cambios en su forma actual' = 4,
-                                  '3. Requiere cambios menores' = 3,
-                                  '2. Requiere cambios mayores' = 2,
-                                  '1. No cumple con lo mínimo' = 1
-  )) %>%
-  # Agrega una nueva columna (score) con el puntaje total de cada evaluador
+  gather(nstudent,student,'Estudiante 1':'Estudiante 2') %>% 
+# Reemplaza las categorías por números (puntaje)
+  mutate_at(vars(5:15),
+    funs(recode(.,
+      '4. No requiere cambios en su forma actual' = 4,
+      '3. Requiere cambios menores' = 3,
+      '2. Requiere cambios mayores' = 2,
+      '1. No cumple con lo mínimo' = 1,
+      .default = 0
+    ))) %>% 
+# Agrega una nueva columna (score) con el puntaje total de cada evaluador
   mutate(score = rowSums(.[5:15])) %>% 
-  # Agrega una nueva columna (grades) con la nota de cada evaluador
+# Agrega una nueva columna (grades) con la nota de cada evaluador
   mutate(grade = (score * 7)/44) %>% 
-  # Muestra la nota para cada estudiante
+# Muestra la nota para cada estudiante
   group_by(student) %>% 
-  summarize(total=mean(grade))
+  summarize(total = mean(grade))
 # Guarda el archivo
-write.csv(grades,'notas.csv')
+write.table(grades, 'notas.csv', sep = ';', dec = ',')
